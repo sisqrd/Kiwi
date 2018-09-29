95c4cbad7c35e222d5dec725e2e619725f161697
git checkout -b sisqrd <sisiyu.design@gmail.com>
2018-09-29 17:19:06 -0400

    we

--- src/frontend/containers/Login/Login.js
+++ src/frontend/containers/Login/Login.js
@@ -3,14 +3,14 @@ import React, {Component} from 'react';
 class Login extends Component {
   render(){
     return (
-      <>
+      
       <form className = "Login">
         <input placeholder="Type in Username"></input>
         <input placeholder="Type in Password"></input>
         <button>Login</button>
         <button>Cancel</button>
       </form>
-    </>
+    
   )
 }
 }
