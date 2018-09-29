1ea8d8359139d695cdf2c025ca7430372e1f5e60
asiopongco <asiopongco@gmail.com>
2018-09-29 16:37:35 -0400

    created register & login

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Layout from './components/Layout/Layout';
+import Register from './containers/Register/Register';
+import Login from './containers/Login/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <Register />
+          <Login />
+        </Layout>
       </div>
     );
   }
