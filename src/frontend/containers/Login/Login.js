32ece18ceb909b80e3c89cd128c0b3ad57d0c072
git checkout -b sisqrd <sisiyu.design@gmail.com>
2018-09-30 11:20:55 -0400

    changes

--- src/frontend/containers/Login/Login.js
+++ src/frontend/containers/Login/Login.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import Auxilary from '../../highorder/Auxilary';
+import { Container,Button,Form, Input} from 'semantic-ui-react'
+
 
 class Login extends Component {
 
   render(){
     return (
       <div className = "Login">
-        <input  placeholder="Type in Username" name="username" onChange = {this.props.handleChange}></input>
-        <input  placeholder="Type in Password" name="password" onChange = {this.props.handleChange}></input>
-        <button onClick={this.props.handleLogin}>Login</button>
-        <button onClick={this.props.onRegisterClick}>Register</button>
+        <Container style={{display:'block',width:'400px', marginTop:"200px", justifyContent:'center', alignItems:'center', height:'500px'}}>
+        <h2 style={{display:'flex', justifyContent:'center'}}> Alert Your Loved Ones </h2>
+        <Input  style={{display:'flex', padding:'10px'}} placeholder="Enter Username" name="username" onChange = {this.props.handleChange}></Input>
+        <Input  style={{display:'flex', padding:'10px'}} placeholder="Enter Password" name="password" onChange = {this.props.handleChange}></Input>
+        <br/>
+          <div style={{display:'flex', justifyContent:'center'}}>
+          <Button style={{width:'190px'}}  basic color='olive' onClick={this.props.handleLogin}>Login</Button>
+          <Button style={{width:'190px'}} basic color='teal' onClick={this.props.onRegisterClick}>Register</Button>
+          </div> 
+        </Container>
     </div>
   )
 }
--- src/frontend/containers/Register/Register.js
+++ src/frontend/containers/Register/Register.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import Auxilary from '../../highorder/Auxilary';
+import { Container,Button,Form, Input} from 'semantic-ui-react'
 
 class Register extends Component {
 
   render(){
     return (
       <div className ="Register">
-        <input  placeholder="Type in Username" name="username" onChange = {this.props.handleChange}></input>
-        <input  placeholder="Type in Password" name="password" onChange = {this.props.handleChange}></input>
-        <input placeholder="Confirm Password" name="confirmPassword" onChange = {this.props.handleChange}></input>
-        <button onClick ={this.props.handleRegister}>Register</button>
-        <button>Cancel</button>
+          <Container style={{display:'block',width:'400px', marginTop:"200px", justifyContent:'center', alignItems:'center', height:'500px'}}>
+          <h2 style={{display:'flex', justifyContent:'center'}}> Join our Kiwi Family </h2>
+        <Input  style={{display:'flex', padding:'10px'}} placeholder="Type in Username" name="username" onChange = {this.props.handleChange}></Input>
+        <Input  style={{display:'flex', padding:'10px'}} placeholder="Type in Password" name="password" onChange = {this.props.handleChange}></Input>
+        <Input style={{display:'flex', padding:'10px'}} placeholder="Confirm Password" name="confirmPassword" onChange = {this.props.handleChange}></Input>
+        <div style={{display:'flex', justifyContent:'center'}}>
+        <Button basic color='olive'style={{width:'190px'}} onClick ={this.props.handleRegister}>Register</Button>
+        <Button  basic color='teal' style={{width:'190px'}} >Cancel</Button>
+        </div>
+        </Container>
     </div>
   )
 }
