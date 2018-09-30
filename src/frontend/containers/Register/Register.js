18ba2a280a33755f1e8adf46f25efc09858a13ed
perrylya <perrylya@gmail.com>
2018-09-29 23:46:16 -0400

    none

--- src/frontend/containers/Register/Register.js
+++ src/frontend/containers/Register/Register.js
@@ -1,16 +1,5 @@
 import React, {Component} from 'react';
-import firebase from 'firebase';
 import Auxilary from '../../highorder/Auxilary';
-
-const config = {
-  apiKey: "AIzaSyCoDCRFGKuNj_oBxQiLrClFlHlBlHkJjAQ",
-  authDomain: "tensile-psyche-218000.firebaseapp.com",
-  databaseURL: "https://tensile-psyche-218000.firebaseio.com",
-  projectId: "tensile-psyche-218000",
-  storageBucket: "tensile-psyche-218000.appspot.com",
-  messagingSenderId: "271934682296"
-};
-firebase.initializeApp(config)
 
 class Register extends Component {
 
@@ -22,6 +11,8 @@ class Register extends Component {
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
   }
+
+
 
   render(){
     return (
