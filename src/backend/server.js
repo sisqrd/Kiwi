4f554227a11c87527b0e92e00397acc9d9d6409c
perrylya <perrylya@gmail.com>
2018-09-29 23:17:15 -0400

    firebase hosting

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const firebase = require("firebase");
+var config = {
+  apiKey: "AIzaSyCoDCRFGKuNj_oBxQiLrClFlHlBlHkJjAQ",
+  authDomain: "tensile-psyche-218000.firebaseapp.com",
+  databaseURL: "https://tensile-psyche-218000.firebaseio.com",
+  projectId: "tensile-psyche-218000",
+  storageBucket: "tensile-psyche-218000.appspot.com",
+  messagingSenderId: "271934682296"
+};
+firebase.initializeApp(config)
 
 const app = express();
 const port = process.env.PORT || 5000;
