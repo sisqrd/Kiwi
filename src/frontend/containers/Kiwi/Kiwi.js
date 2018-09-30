65836806c47ac9560abe62784801bb06c077d7f5
asiopongco <asiopongco@gmail.com>
2018-09-30 01:34:39 -0400

    started content display

--- src/frontend/containers/Kiwi/Kiwi.js
+++ src/frontend/containers/Kiwi/Kiwi.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+let arr = [{
+  name: 'Perry',
+  relationship: 'friend',
+  number: '+12405996788',
+  message: 'yeeeeeeer'}]
 
 class Kiwi extends Component {
-  
 
 
   render(){
